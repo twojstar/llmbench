4c5d9367557a016a413fafe2549258a324066e4c
trvny <120686325+trvny@users.noreply.github.com>
2026-09-08 13:42:56 +0200

    build: wire Unicode normalization into shared

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,6 +28,7 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotaml)
+            implementation(libs.normalize)
         }
         androidMain.dependencies {
             implementation(libs.jtokkit)
